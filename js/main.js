159b9cec0d16d70bd403cf18dbd9f1d97968b02a
LauraAmaya08 <laurasofiaamayacasado@gmail.com>
2024-08-08 22:52:49 -0500

    feat: :tada: Starting a project!

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    const api = fetch('https://fakestoreapi.com/products')
+
+     async function cargarApi(){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+})
