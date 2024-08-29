c68dcc54d28ac9cff0cc2320c66e72d3cda8dfd0
LauraAmaya08 <laurasofiaamayacasado@gmail.com>
2024-08-29 17:17:40 -0500

    feat: :sparkles: Show products and load api's information completed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,43 @@
 document.addEventListener("DOMContentLoaded", () =>{
-    const api = fetch('https://fakestoreapi.com/products')
-
-     async function cargarApi(){
-        try {
-            
-        } catch (error) {
-            
-        }
+    if(window.location.pathname.includes("index")){
+        mostrarTodosLosProductos()
     }
 })
+
+const api = 'https://fakestoreapi.com/products'
+
+const cargarData = async() => {
+    try {
+        const data = await fetch(api)
+        if (data.ok){
+            dataJson = await data.json()
+            return dataJson
+        }else{
+            throw new Error ("Error conectando a la API: "+ data.statusText)
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const mostrarTodosLosProductos = async() => {
+    try {
+        const data = await cargarData()
+        console.log(data)
+        let contenedor = document.getElementById("contenedorProductos");
+        let html = ""
+        data.forEach(dato => {
+            info = `<div class="productoContainer">
+            <img class="imagenProducto" src="${dato.image}" alt= "Imagen del producto">
+            <h1 class="productoNombre">${dato.title}</h1>
+            <p class="productoDescrip">${dato.description}<p>
+            <p class="productoRate">Calificación: ${dato.rating.rate}<p>
+            <p class="productoPrecio">${dato.price}<p>
+            </div>`
+            html += info
+            contenedor.innerHTML = html
+        });
+    } catch (error) {
+        console.error(error)
+    }
+}
